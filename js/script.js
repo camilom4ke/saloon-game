55022aa5ea3684c058b0bd60b3d4ae2b490e054a
Camilosorio <tech@camilo.fr>
2019-10-31 10:14:55 +0100

    Adding players objects.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 // define players objects
+const brajer = {
+    name: "Brajer",
+    disponibility: "Malheureusement disponible",
+    disponibilityStatus: true,
+    image: "images/Brajer.png",
+    quote: "Hey, je prépare un nouveau festoch, tu viens ?",
+    healthLevel: "faible"
+}
+
+const metiah = {
+    name: "Metiah",
+    disponibility: "disponible",
+    disponibilityStatus: true,
+    image: "images/H.png",
+    quote: "Qui a mangé mon flan coco pendant que je dormais ?",
+    healthLevel: "modérée"
+}
+
+const grillz = {
+    name: "Grillz",
+    disponibility: "indisponible - a débloquer",
+    disponibilityStatus: false,
+    image: "images/Screenshot_7.png",
+    quote: "Alfred ? T'es où ? Quelqu'un a vu mon assistant ?",
+    healthLevel: "élévée"
+}
+
+const nasser = {
+    name: "Nasser",
+    disponibility: "personnage légendaire - a débloquer",
+    disponibilityStatus: false,
+    image: "images/Nasser.png",
+    quote: "Hey Snedz, c'est qui le boss de la night ?",
+    healthLevel: "légendaire"
+}
 
 
 // define ennemies objeccts 
@@ -8,6 +43,10 @@
 
 
 // function choice
+
+function choosePlayer(player) {
+
+}
 
 
 // 
